d27bd92fcb50fb80f85e1c027ba91a701bf63c26
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2024-09-09 12:11:50 +0500

    feat: update  DatePicker

--- layout/Main/index.tsx
+++ layout/Main/index.tsx
@@ -3,15 +3,12 @@ import { ReactNode } from "react";
 import Navbar from "layout/Navbar";
 import Sidebar from "layout/Sidebar";
 import { LayoutWrapper, StyledContent } from "./style";
-import { DatePicker } from "components";
-import { useForm } from "react-hook-form";
 
 type MainLayoutProps = {
   children: ReactNode;
 };
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-  const { control } = useForm();
   return (
     <LayoutWrapper>
       <Sidebar />
@@ -19,7 +16,6 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
         <Navbar />
         <StyledContent>{children}</StyledContent>
       </Layout>
-      <DatePicker control={control} name="date" label="choose smth" />
     </LayoutWrapper>
   );
 };
