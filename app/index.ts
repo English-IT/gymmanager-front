f73a96d8816a3267343f24ac4a956d121cdaacc8
alisherova <alisherovanurjahon@gmail.com>
2024-09-18 14:29:10 +0200

    feat: add loading component

--- app/index.ts
+++ app/index.ts
@@ -2,3 +2,4 @@ export { default as GymRegistration } from "./gymRegistration";
 export { default as Login } from "./login";
 export { default as ResetPasword } from "./resetPassword";
 export { default as Users } from "./users";
+export { default as Loader } from "./Loader";
