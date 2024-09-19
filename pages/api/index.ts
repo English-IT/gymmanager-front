726bb716b66aeeb25b78d79ff161e060ce8be813
alisherova <alisherovanurjahon@gmail.com>
2024-09-19 12:51:10 +0200

    implement users get api

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,1 +1,14 @@
+import axios from "axios";
+
 export const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+export const http = axios.create({
+  baseURL: baseURL,
+  headers: {
+    "Content-type": "application/json",
+    Accept: "application/json",
+    Authorization: "",
+    "Access-Control-Allow-Origin": "*",
+  },
+  withCredentials: true,
+});
---
+++ pages/gym-managers.tsx
@@ -0,0 +1,11 @@
+import GymManagers from 'app/gymManagers'
+import React from 'react'
+
+const GymMangers = () => {
+
+    return (
+        <GymManagers />
+    )
+}
+
+export default GymMangers
